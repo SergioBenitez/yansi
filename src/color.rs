23a6069d67a510fd6875d7da1ac0a644d8912a79
Sergio Benitez <sb@sergio.bz>
2022-12-13 16:32:32 +0100

    Free changes from cargo fix.

--- src/color.rs
+++ src/color.rs
@@ -70,7 +70,7 @@ impl Color {
         Style::new(self)
     }
 
-    pub(crate) fn ansi_fmt(&self, f: &mut fmt::Write) -> fmt::Result {
+    pub(crate) fn ansi_fmt(&self, f: &mut dyn fmt::Write) -> fmt::Result {
         match *self {
             Color::Unset => Ok(()),
             Color::Default => write!(f, "9"),
--- src/style.rs
+++ src/style.rs
@@ -181,7 +181,7 @@ macro_rules! checker_for {
 }
 
 #[inline]
-fn write_spliced<T: Display>(c: &mut bool, f: &mut fmt::Write, t: T) -> fmt::Result {
+fn write_spliced<T: Display>(c: &mut bool, f: &mut dyn fmt::Write, t: T) -> fmt::Result {
     if *c {
         write!(f, ";{}", t)
     } else {
@@ -408,7 +408,7 @@ impl Style {
     ///     }
     /// }
     /// ```
-    pub fn fmt_prefix(&self, f: &mut fmt::Write) -> fmt::Result {
+    pub fn fmt_prefix(&self, f: &mut dyn fmt::Write) -> fmt::Result {
         // A user may just want a code-free string when no styles are applied.
         if self.is_plain() {
             return Ok(());
@@ -470,7 +470,7 @@ impl Style {
     ///     }
     /// }
     /// ```
-    pub fn fmt_suffix(&self, f: &mut fmt::Write) -> fmt::Result {
+    pub fn fmt_suffix(&self, f: &mut dyn fmt::Write) -> fmt::Result {
         if self.is_plain() {
             return Ok(());
         }
